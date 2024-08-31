40c65dc61dff1d1b3a24f4fe41f3ebc680cad239
Stavros <steveiliop56@gmail.com>
2024-08-31 20:23:58 +0300

    feat: add stop command

---
+++ cmd/stop.go
@@ -0,0 +1,43 @@
+package cmd
+
+import (
+	"fmt"
+	"os/exec"
+
+	"github.com/spf13/cobra"
+	"github.com/steveiliop56/runtipi-cli-go/internal/spinner"
+)
+
+func init() {
+	rootCmd.AddCommand(stopCmd)
+}
+
+var stopCmd = &cobra.Command{
+	Use: "stop",
+	Short: "Stop runtipi",
+	Long: "Use this command to stop the runtipi docker stack",
+	Run: func(cmd *cobra.Command, args []string) {
+		// Stop containers
+		spinner.SetMessage("Stopping containers")
+		spinner.Start()
+
+		_, err := exec.Command("docker", "compose", "down", "--remove-orphans", "--rmi", "local").Output()
+
+		if err != nil {
+			spinner.Fail("Error in stopping containers")
+			spinner.Stop()
+			fmt.Printf("Error: %s\n", err)
+			return
+		}
+
+		containersToRm := []string{"runtipi-reverse-proxy", "runtipi-db", "runtipi-redis", "runtipi", "tipi-db", "tipi-redis", "tipi-reverse-proxy", "tipi-docker-proxy", "tipi-dashboard", "tipi-worker"}
+
+		for _, container := range containersToRm {
+			exec.Command("docker", "stop", container).Output()
+			exec.Command("docker", "rm", container).Output()
+		}
+
+		spinner.Succeed("Runtipi stopped successfully")
+		spinner.Stop()
+	},
+}
