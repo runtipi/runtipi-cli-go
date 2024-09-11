b306a78105f129703f5b05d329186e79404709b5
Stavros <steveiliop56@gmail.com>
2024-09-11 11:19:36 +0300

    feat: list-backups command

--- cmd/list-backups.go
+++ cmd/list-backups.go
@@ -1,9 +1,10 @@
-package subcommands
+package cmd
 
 import (
 	"fmt"
 	"os"
 	"path"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
@@ -14,11 +15,14 @@ import (
 	"github.com/steveiliop56/runtipi-cli-go/internal/utils"
 )
 
-var	ListAppBackupsCmd = &cobra.Command{
-	Use: "list-backups [app]",
-	Short: "List app backups",
-	Long: "Lists all the backups of an app",
-	Args: cobra.MinimumNArgs(1),
+func init() {
+	rootCmd.AddCommand(listBackupsCmd)
+}
+
+var listBackupsCmd = &cobra.Command{
+	Use: "list-backups",
+	Short: "List Runtipi backups",
+	Long: "Use this command to list all the available Runtipi backups",
 	Run: func(cmd *cobra.Command, args []string) {
 		// Get root folder
 		rootFolder, osErr := os.Getwd()
@@ -30,15 +34,7 @@ var	ListAppBackupsCmd = &cobra.Command{
 		}
 
 		// Define paths
-		backupPath := path.Join(rootFolder, "backups", args[0])
-
-		// Check if folder exists
-		_, pathCheckErr := os.Stat(backupPath)
-		if pathCheckErr != nil {
-			fmt.Printf("%s App backup path doesn't exist\n", constants.Red("✗"))
-			fmt.Printf("Error: %s\n", pathCheckErr)
-			os.Exit(1)
-		}
+		backupPath := path.Join(rootFolder, "backups")
 
 		// Read directory
 		backups, readErr := os.ReadDir(backupPath)
@@ -51,12 +47,21 @@ var	ListAppBackupsCmd = &cobra.Command{
 		// List backups
 		backupsTable := table.New(os.Stdout)
 		backupsTable.SetHeaders("Name", "Size", "Date Created")
+		backupNum := 0
 		for _, backup := range backups {
-			backupInfo, _ := backup.Info()
-			backupSize := utils.FormatFileSize(float64(backupInfo.Size()))
-			backupDateIso, _ := strconv.ParseInt(strings.Replace(strings.Replace(strings.Replace(backup.Name(), args[0], "", 1), ".tar.gz", "", 1), "-", "", 1), 10, 64)
-			backupDate := time.UnixMilli(backupDateIso).String()
-			backupsTable.AddRow(backup.Name(), backupSize, backupDate)
+			if !backup.IsDir() {
+				backupInfo, _ := backup.Info()
+				backupSize := utils.FormatFileSize(float64(backupInfo.Size()))
+				backupDateIso, _ := strconv.ParseInt(strings.Replace(strings.Replace(strings.Replace(backup.Name(), filepath.Base(rootFolder), "", 1), ".tar.gz", "", 1), "-", "", 1), 10, 64)
+				backupDate := time.UnixMilli(backupDateIso).String()
+				backupsTable.AddRow(backup.Name(), backupSize, backupDate)
+				backupNum += 1
+			}
+		}
+
+		if backupNum == 0 {
+			fmt.Printf("%s No backups found\n", constants.Red("✗"))
+			os.Exit(0)
 		}
 
 		backupsTable.Render()
