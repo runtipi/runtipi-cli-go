023c2476792fb6cb931a98524e4fc8aa8c71a1d9
Stavros <steveiliop56@gmail.com>
2024-09-22 20:42:32 +0300

    refactor(utils): simplify format file size function

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -2,20 +2,14 @@ package utils
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func FormatFileSize(fileSize float64) (string) {
-	if fileSize < (1<<10) {
-		return fmt.Sprintf("%sB", strconv.Itoa(int(fileSize)))
-	} else if fileSize > (1<<10) && fileSize < (1<<20) {
-		return fmt.Sprintf("%.2fKB", fileSize/(1<<10))
-	} else if fileSize > (1<<20) && fileSize < (1<<30) {
-		return fmt.Sprintf("%2.fMB", fileSize/(1<<20))
-	} else if fileSize > (1<<30) && fileSize < (1<<40) {
-		return fmt.Sprintf("%.2fGB", fileSize/(1<<30))
-	} else if fileSize > (1<<40) {
-		return fmt.Sprintf("%.2fTB", fileSize/(1<<40))
+	sizes := []string{"B", "KB", "MB", "GB", "TB"}
+	i := 0
+	for fileSize >= 1024 && i < len(sizes)-1 {
+		fileSize /= 1024
+		i++
 	}
-	return fmt.Sprintf("%sB", strconv.Itoa(int(fileSize)))
+	return fmt.Sprintf("%.2f%s", fileSize, sizes[i])
 }
